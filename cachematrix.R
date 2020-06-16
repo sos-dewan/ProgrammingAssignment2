41fe64c6f467a996d9db40ec1e1e3bf14f90d103
Shashank Dewan <dewan.shashank@gmail.com>
2020-06-16 22:36:12 +0545

    20200616

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,9 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These function makes the use of lexical scoping in R, which helps to cache the 
+## required value and use it later instead of calculating the value again
 
-## Write a short comment describing this function
+## This first function takes a matrix as an argument and calculates it's inverse and 
+## caches the value. The function is similar to example in vector, and I have made
+## the necessary changes in variable names, data type and functions called.
 
 makeCacheMatrix <- function(x = matrix()) 
 {
@@ -21,7 +23,9 @@ makeCacheMatrix <- function(x = matrix())
 }
 
 
-## Write a short comment describing this function
+## This functions takes in an argument x (matrix in our case) and tries to get
+## its inverse if there exists one. But if the inverse is null then this function
+## calculates the inverse and returns the inverse
 
 cacheSolve <- function(x, ...) 
 {
@@ -29,7 +33,7 @@ cacheSolve <- function(x, ...)
     m <- x$getInverse()
     if(!is.null(m))
     {
-        message("gettgin cached data")
+        message("getting cached data")
         return(m)
     }
     mat <- x$get()
